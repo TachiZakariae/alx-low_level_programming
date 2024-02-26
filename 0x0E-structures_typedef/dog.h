39a8a613901d75186eabf1335f1011d0ba7c77cd
TachiZakariae <zakariaetachi@gmail.com>
2024-02-26 15:27:34 -0800

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct
+ * @name: dog's name
+ * @arg: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct i a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float arg;
+	char *owner;
+};
+#endif
