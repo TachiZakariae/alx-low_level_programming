d48d6e85998afb8d41a80ea9b8effe7d8c10873f
TachiZakariae <zakariaetachi@gmail.com>
2024-07-08 16:17:26 -0700

    C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+
+/**
+ * free_dlistint - free a dlist
+ * @head: pointer to current head node
+ *
+ * Return:
+ * void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
