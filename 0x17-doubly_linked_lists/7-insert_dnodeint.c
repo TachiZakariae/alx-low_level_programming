879fa94b3bcc0236924cd752652d3d2a5b13fa91
TachiZakariae <zakariaetachi@gmail.com>
2024-07-12 08:03:29 -0700

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,13 +2,13 @@
 
 /**
  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- * 				list at a given position.
+ * 			      list at a given position.
  * @h: A pointer to the head of the dlistint_t list.
  * @idx: The position to insert the new node.
- * @n: The integer for the new mode to contain.
+ * @n: The integer for the new node to contain.
  *
  * Return : If the function fails - NULL.
- * 		Otherwise - the address of the new node.
+ * 	    Otherwise - the address of the new node.
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -26,6 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
